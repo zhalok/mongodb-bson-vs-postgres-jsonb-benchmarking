787e2dc01644d738f75bdf91d96350ee4d55ebeb
zhalok <rahmanzhalok@gmail.com>
2026-07-17 14:37:49 +0600

    added logger middleware

--- utils/bootstrap.js
+++ utils/bootstrap.js
@@ -7,8 +7,20 @@ import {
   startDeliveryHandler,
   completeOrderHandler,
 } from "../services/orders.service.js";
+function loggerMiddleware(req, res, next) {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const durationMs = Date.now() - start;
+    console.log(`${req.method} ${req.originalUrl} ${res.statusCode} ${durationMs}ms`);
+  });
+
+  next();
+}
+
 export function createApp(ordersRepository) {
   const app = express();
+  app.use(loggerMiddleware);
   app.use(express.json());
 
   app.post("/orders", (req, res) => createOrderHandler(req, res, ordersRepository));
