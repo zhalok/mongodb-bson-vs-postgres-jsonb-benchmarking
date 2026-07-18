384153f742e93d78e535332de12028cf139b4899
zhalok <rahmanzhalok@gmail.com>
2026-07-18 10:22:16 +0600

    refactored the client and server into proper directories

--- mongo.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const client = new MongoClient(`mongodb://mongo:mongo@${process.env.HOST || "localhost"}:27017`);
-
-await client.connect();
-
-export const db = client.db("jsonb_experiments");
